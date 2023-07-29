64dbdef98fbbd4be530cd6535a67d875967f5564
TomeRoque <127785416+TomeRoque@users.noreply.github.com>
2023-07-29 15:42:23 +0100

    Delete portfolio directory

--- portfolio/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Cadeira
-from .models import Portfolio
-
-admin.site.register(Portfolio)
-admin.site.register(Cadeira)
-
